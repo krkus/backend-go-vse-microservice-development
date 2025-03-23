029d16ff5024db2a98ddd526fb2e5736fbb49cfa
Milan Černý <milancerny2@seznam.cz>
2025-03-23 20:47:15 +0100

    zmeny

--- 04/transport/api/v1/user.go
+++ 04/transport/api/v1/user.go
@@ -2,8 +2,10 @@ package v1
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-playground/validator/v10"
@@ -19,6 +21,14 @@ func getEmailFromURL(r *http.Request) string {
 	return email
 }
 
+func ValidateBirthDate(bd model.BirthDate) error {
+	dateStr := fmt.Sprintf("%04d-%02d-%02d", bd.Year, bd.Month, bd.Day)
+	if _, err := time.Parse("2006-01-02", dateStr); err != nil {
+		return err
+	}
+	return nil
+}
+
 func (h *Handler) CreateUser(w http.ResponseWriter, r *http.Request) {
 	b, err := io.ReadAll(r.Body)
 	if err != nil {
@@ -31,10 +41,14 @@ func (h *Handler) CreateUser(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	if err := validate.Struct(user); err != nil {
-		//validace tady???
 		util.WriteErrResponse(w, http.StatusBadRequest, err)
 		return
 
+	}
+
+	if err := ValidateBirthDate(user.BirthDate); err != nil {
+		util.WriteErrResponse(w, http.StatusBadRequest, err)
+		return
 	}
 
 	if err := h.service.CreateUser(r.Context(), user); err != nil {
